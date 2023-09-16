3ad0958eaaa1e8786246c473c4993ab7b8615ad8
Zhang Jiashu <zhangjiashu2010@gmail.com>
2023-09-17 04:21:26 +0800

    改回原来的base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base: '/breast-calculator/',
   plugins: [vue()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
