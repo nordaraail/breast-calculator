9cf4117099a44dc133ee14705068b15c9ec02eae
Zhang Jiashu <zhangjiashu2010@gmail.com>
2023-09-17 22:06:36 +0800

    再次修改依赖

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from "vite";
-// import vue from '@vitejs/plugin-vue' // vue 3
-import vue from "@vitejs/plugin-vue2"; //vue 2
 import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
+import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/breast-calculator/',
-  plugins: [vue()],
+  base:'/breast-calculator/',
+  plugins: [
+    vue2(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+    })
+  ],
   resolve: {
     base:'breast-calculator',
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
