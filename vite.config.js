a2bcdfcaf17da27f1028955add560ea0aeb070fe
Zhang Jiashu <zhangjiashu2010@gmail.com>
2023-09-17 09:56:20 +0800

    修改@指向

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 // import vue from '@vitejs/plugin-vue' // vue 3
 import { createVuePlugin as vue } from "vite-plugin-vue2"; //vue 2
-const path = require("path");
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
